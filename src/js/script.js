c5dad58266248f3e70f6911729ee3ed45b7365ce
kanan <kanansnote@gmail.com>
2024-04-17 01:09:21 +0400

    Lots of work on all sections, with a few incomplete parts of styling.

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.ourFeedbacks-Center', {
+    slidesPerView: '1', // Adjust this value to control how many slides are visible at a time
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
