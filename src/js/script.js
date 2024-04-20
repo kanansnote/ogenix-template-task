7dc0d896ac4bf6b3b97bc808b20dd66611e5dd9c
kanan <kanansnote@gmail.com>
2024-04-20 15:38:28 +0400

    Implemented Back to Top button.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,21 @@
-const goTop = document.getElementById('goTop');
+const topBtn = document.getElementById("topBtn");
 
-goTop.addEventListener('click', function () {
-    
-})
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";  /* Show button when scrolled down */
+  } else {
+    topBtn.style.display = "none";   /* Hide button when scrolled up */
+  }
+}
+
+topBtn.addEventListener("click", function() {
+  document.documentElement.scrollIntoView({
+    behavior: 'smooth',  // Enables smooth scrolling animation
+    block: 'start'       // Scrolls to the top of the page
+  });
+});
 
 const swiper = new Swiper('.ourFeedbacks-Center', {
     slidesPerView: '1', // Adjust this value to control how many slides are visible at a time
